298a55899aaf645fa43f06f140aa35ea19ed7c15
Sandra Tjushka <sandratjushka@gmail.com>
2026-06-18 02:14:50 +0300

    Vormitöötlus Reactis

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
     const expenses = [
@@ -17,6 +18,7 @@ const App = () => {
 
     return (
         <div className="App">
+            <NewExpense></NewExpense>
             <ExpenseItem data={expenses[0]}/>
             <ExpenseItem data={expenses[1]}/>
         </div>
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import ExpenseForm from "./ExpenseForm"
+
+const NewExpense = (props) => {
+    return(
+        <div className="new-expense">
+            <ExpenseForm />
+        </div>
+    )
+}
+
+export default NewExpense
