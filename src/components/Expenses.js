3604491a198e3dc733a1f8574249618c78bd9406
Sandra Tjushka <sandratjushka@gmail.com>
2026-06-18 01:28:57 +0300

    Komponentide kompositsioon ja info edastamine

---
+++ src/components/Expenses.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+
+    return(
+        <Card className="expenses">
+            <ExpenseItem data={props.expenses[0]}/>
+            <ExpenseItem data={props.expenses[0]}/>
+        </Card>
+    )
+}
+
+export default Expenses
